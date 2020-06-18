76f36f6a365a29d80bbc01d0ddb3ccbddec10d73
Connor Holland <connorholland@Connors-MacBook-Pro.local>
2020-06-18 16:23:39 -0600

    complete

---
+++ CocktailAPI/CocktailAPI/CustomView/CocktailTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CocktailTableViewCell.swift
+//  CocktailAPI
+//
+//  Created by Connor Holland on 6/17/20.
+//  Copyright © 2020 Connor Holland. All rights reserved.
+//
+
+import UIKit
+
+class CocktailTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ CocktailAPI/CocktailAPI/Models/CocktailError.swift
@@ -0,0 +1,9 @@
+//
+//  CocktailError.swift
+//  CocktailAPI
+//
+//  Created by Connor Holland on 6/17/20.
+//  Copyright © 2020 Connor Holland. All rights reserved.
+//
+
+import Foundation
